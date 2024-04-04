6ada8e5cc32afad05a0ff8413836473dadc76015
payals363 <165778302+payals363@users.noreply.github.com>
2024-04-04 09:24:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,20 +7,17 @@ terraform {
  }
 }
 
-provider "docker" 
-{
+provider "docker" {
  host = "unix:///var/run/docker.sock"
 }
 
 # Pulls the image
-resource "docker_image" "nginx" 
-{
+resource "docker_image" "nginx" {
  name = "nginx:latest"
 }
 
 # Create a container
-resource "docker_container" "foo" 
-{
+resource "docker_container" "foo" {
  image = docker_image.nginx.latest
  name = "foo"
 }
